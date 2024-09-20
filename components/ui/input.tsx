73237c62c564b7700f0e4e5ea5b0c20522112da9
Dhimas <dhimas.200180122@mhs.unimal.ac.id>
2024-09-20 16:54:51 +0700

    udpate

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,17 +1,20 @@
-import * as React from "react";
-
 import { cn } from "@/lib/utils";
+import React from "react";
 
-export type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
+export interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  foo?: boolean;
+}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, ...props }, ref) => {
+  ({ className, type, foo = false, ...props }, ref) => {
     return (
       <input
         type={type}
         className={cn(
-          "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
-          className
+          "flex h-10 w-full rounded-md border-hidden border-input bg-card px-3 py-2 text-sm shadow-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-0 disabled:cursor-not-allowed disabled:opacity-50",
+          className,
+          foo && "bg-red-500"
         )}
         ref={ref}
         {...props}
